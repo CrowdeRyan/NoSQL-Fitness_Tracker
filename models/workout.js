596972fcf2ca92a2b875d63f945452119fa06147
Ryan Crowder <crowderyan@gmail.com>
2021-08-27 11:10:56 -0400

    adding title and duration

--- models/workout.js
+++ models/workout.js
@@ -4,4 +4,11 @@ const Schema = mongoose.Schema;
 
 module.exports = mongoose.model("Workout", workoutSchema);
 
-const ExcerciseSchema = new Schema({});
+const ExcerciseSchema = new Schema({
+  type: String,
+  name: String,
+  duration: {
+    type: Number,
+    min: [0, "Negative number is not allowed"],
+  },
+});
