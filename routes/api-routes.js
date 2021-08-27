0901e4b4ad9bc532f5e2703558d33aa6d2de260b
Ryan Crowder <crowderyan@gmail.com>
2021-08-27 11:23:11 -0400

    adding create route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,4 +15,29 @@ module.exports = function (app) {
         res.status(500).json(err);
       });
   });
+  app.put("/api/workouts/:id", (req, res) => {
+    console.log("*** put /api/workouts/:id is called", req.body);
+
+    var now = new Date();
+    var todayStart = new Date();
+    var todayEnd = new Date();
+    todayStart.setHours(0, 0, 0, 0);
+    todayEnd.setHours(23, 59, 59, 999);
+
+    console.log("*** req.body ***", req.body);
+    var tempJSON = {
+      day: now,
+      exercises: [
+        {
+          type: req.body.type,
+          name: req.body.name,
+          duration: req.body.duration,
+          distance: req.body.distance,
+          weight: req.body.weight,
+          reps: req.body.reps,
+          sets: req.body.sets,
+        },
+      ],
+    };
+  });
 };
