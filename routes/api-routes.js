84556a1cc861142a762983cfac1e4926adb414e5
Ryan Crowder <crowderyan@gmail.com>
2021-08-27 11:21:15 -0400

    adding get route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,18 @@
 const db = require("../models");
 
-module.exports = function (app) {};
+module.exports = function (app) {
+  app.get("/api/workouts", (req, res) => {
+    console.log("get /api/workouts is called.");
+    db.Workout.find({})
+      .sort({ day: "asc" })
+      .then((dbResult) => {
+        //console.log(dbResult);
+        console.log("find ok");
+        res.json(dbResult);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+};
