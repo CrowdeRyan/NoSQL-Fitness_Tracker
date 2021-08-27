95d8375fe4a12d9353a5f0ba052b7cf4d18936e7
Ryan Crowder <crowderyan@gmail.com>
2021-08-27 11:29:04 -0400

    connect exercise

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,8 @@
 const path = require("path");
 
-module.exports = function (app) {};
+module.exports = function (app) {
+  app.get("/exercise", function (req, res) {
+    //console.log("get /exercise")
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+};
