3b63f56da7079a8d695f7eb84abdfb2c485dc8df
Ryan Crowder <crowderyan@gmail.com>
2021-08-30 02:40:22 -0400

    connect mongodb

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
