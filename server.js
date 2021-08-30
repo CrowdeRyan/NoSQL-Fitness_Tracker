b3864d3705196b5fbd79716608091d48f554636f
Ryan Crowder <crowderyan@gmail.com>
2021-08-30 02:39:45 -0400

    added requires and app cals

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
